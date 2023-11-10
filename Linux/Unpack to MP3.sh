f5fb3bab5dce469ad6bdcaa6c4df2102885dc5bf
VIPLightning <Devon1994@aol.com>
2023-11-10 01:29:43 -0500

    Third and Fourth Shell Scripts

---
+++ Linux/Unpack to MP3.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+./Tools/quickbms  "Tools/wavescan.bms" "Game Files" "Tools/Decoding"
+for c in Tools/Decoding/*.wem; do "./Tools/vgmstream-cli" -o "?f.wem2" $c; done
+for d in Tools/Decoding/*.wem2; do mv "$d" "MP3"; done
+for f in MP3/*.wem2 do ffmpeg -i $f -acodec libmp3lame -q:a 0 -y "MP3/%%~nd.mp3"; done
+rm MP3/*.wem2
+
+cat tornado.txt
+
+echo
+echo "-------------------------------------------------------------"
+
+echo "Unpack finished! Files should be in the 'MP3' folder"
+
+echo "-------------------------------------------------------------"
+echo
+
+echo 
+
+read -p "Should I delete BNKs and PCKs from the Game Files folder? [Y/N]" ans;
+case $ans in
+  y|Y) 
+     for e in "Game Files/*.PCK"; do rm $e; done 
+     echo "Files deleted, enjoy your unpacked audio! -/u/Vextil ;)";;
+     
+  n|N)
+     echo "BNKs and PCKs kept, enjoy your unpacked audio! -/u/Vextil ;)";;
+  *) 
+     echo "invalid option";;
+esac
+     
+ 
+if [ $? -eq 0 ]
+then
+   echo "Exit code returned 0! Successful!"
+else 
+   echo "Exit code returned $?! Houston we have a problem!"
+fi
---
+++ Linux/Unpack to OGG.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+./Tools/quickbms  "Tools/wavescan.bms" "Game Files" "Tools/Decoding"
+for c in Tools/Decoding/*.wem; do ./Tools/ww2ogg "$c" --pcb Tools/packed_codebooks.bin; done
+rm Tools/Decoding/*.wem
+for d in Tools/Decoding/*.OGG; do revorb "$d"; done
+mv Tools/Decoding/*.OGG "OGG"
+cat tornado.txt
+
+echo
+echo "-------------------------------------------------------------"
+
+echo "Unpack finished! Files should be in the 'MP3' folder"
+
+echo "-------------------------------------------------------------"
+echo
+
+echo 
+
+read -p "Should I delete BNKs and PCKs from the Game Files folder? [Y/N]" ans;
+case $ans in
+  y|Y) 
+     for e in "Game Files/*.PCK"; do rm $e; done 
+     echo "Files deleted, enjoy your unpacked audio! -/u/Vextil ;)";;
+     
+  n|N)
+     echo "BNKs and PCKs kept, enjoy your unpacked audio! -/u/Vextil ;)";;
+  *) 
+     echo "invalid option";;
+esac
+     
+ 
+if [ $? -eq 0 ]
+then
+   echo "Exit code returned 0! Successful!"
+else 
+   echo "Exit code returned $?! Houston we have a problem!"
+fi
